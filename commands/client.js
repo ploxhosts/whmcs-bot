2ed2f886f2bf1706976d1cfc25817b356b4e76bd
Mark David <44349634+markd69@users.noreply.github.com>
2019-11-21 20:43:46 -0500

    Update client.js

--- commands/client.js
+++ commands/client.js
@@ -2,11 +2,23 @@ const config = require(`../config.json`);
 const Discord = require(`discord.js`);
 const whmcsGet = require('../functions/whmcsGet.js');
 const sql = require('sqlite');
+var request = require('request');
 sql.open('./bot.sqlite');
+var authOptions = {
+    url: 'https://billing.plox.host/includes/api.php',
+    headers: {
+            'action' : 'GetClientsDetails',
+            'username' : '8Vl3kElZ3z8YEJO2v2YMwRySkpK1xs46',
+            'password' : 'no',
+            'clientid' : '2439',
+            'stats' : true,
+            'responsetype' : 'json',
+    },
+  };
 
 // welp, this is just great...
 exports.run = async (client, msg, args) => {
-    let member = msg.mentions.members.first();
+/*    let member = msg.mentions.members.first();
     if (!member) return Embed(msg.channel, `You must mention the member you wish to get the information of.`, 'error', 'Error');
         let clientid = await sql.get(`SELECT * FROM whmcs WHERE discordId = "${member.id}"`);
     let value = await whmcsGet.get({}, 'GetClientsdetails', member);
@@ -19,9 +31,14 @@ exports.run = async (client, msg, args) => {
     if (!row || clientUser.length === 0) return Embed(msg.channel, `${member} does not have a WHMCS account or did not link it.`, 'error', 'Error');
     clientUser = clientUser[0];
     Embed(msg.channel, `Email: ${clientUser['email']}\nFull name: ${clientUser['firstname']} ${clientUser['lastname']}`, 'main', `${member.user.username}#${member.user.discriminator} Client Info`)
-};
+*/ };
 
-function Embed(channel, description, color, title) {
+// dev code
+  request.post(authOptions, function(error, response, body) {
+    console.log(body);
+  });
+
+/* function Embed(channel, description, color, title) {
     let embedBuild = new Discord.RichEmbed()
         .setTitle(title)
         .setColor(config.embed.colors[color])
@@ -29,7 +46,8 @@ function Embed(channel, description, color, title) {
         .setTimestamp()
         .setFooter(config.embed.footer);
     channel.send(embedBuild);
-}
+    }
+*/
 
 exports.conf = {
     enabled: true,
