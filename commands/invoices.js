c7d16ca68a4a80813773b97e21d3628e6d9c2930
Mark David <44349634+markd69@users.noreply.github.com>
2019-11-23 10:37:13 -0500

    Update invoices.js

--- commands/invoices.js
+++ commands/invoices.js
@@ -6,15 +6,12 @@ sql.open('./bot.sqlite');
 
 exports.run = async (client, msg, args) => {
     let member = msg.mentions.members.first();
-    if (!member) return Embed(msg.channel, `You must mention the member you wish to get the invoices of.`, 'error', 'Error');
-    let value = await whmcsGet.get({}, 'GetInvoices', member);
-    if (value === undefined || !value.invoices || value.invoices.length === 0 || value.invoices.invoice.length === 0) return Embed(msg.channel, `There was an error performing this command.`, 'error', 'Error');
-    let invoices = value.invoices.invoice;
-    let descriptions = [];
-    for (let i = 0; i < invoices.length; i++) {
-        let description = await whmcsGet.get({invoiceid: invoices[i]['id']}, 'GetInvoice', member);
-        descriptions.push(description.items.item[0].description);
-    }
+    let row = await sql.get(`SELECT * FROM whmcs WHERE discordId = "${member.id}"`);
+    if (!row) return;
+    let clientUser = await whmcsGet.get({ clientid: row.clientId }, 'getInvoices');
+    let invoices = clientUser.invoices.invoice;
+        let descriptions = [];
+    if (!clientUser) return Embed(msg.channel, `${member} does not have a WHMCS account or did not link it.`, 'error', 'Error');
     Embed(msg.channel, `${invoices.map(i => `ID: ${i['id']}\nDescription: ${descriptions[invoices.indexOf(i)]}\nDue: ${i['duedate']}`).join('\n\n')}`, 'main', `${member.user.username}#${member.user.discriminator} Tickets`)
 };
 
