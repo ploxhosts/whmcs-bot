4cfad7f71c5de8e5d894749c40bcdb11b8f503ad
Mark David <44349634+markd69@users.noreply.github.com>
2019-11-23 10:36:48 -0500

    Update client.js

--- commands/client.js
+++ commands/client.js
@@ -4,20 +4,13 @@ const whmcsGet = require('../functions/whmcsGet.js');
 const sql = require('sqlite');
 sql.open('./bot.sqlite');
 
-// welp, this is just great...
 exports.run = async (client, msg, args) => {
     let member = msg.mentions.members.first();
     if (!member) return Embed(msg.channel, `You must mention the member you wish to get the information of.`, 'error', 'Error');
-        let clientid = await sql.get(`SELECT * FROM whmcs WHERE discordId = "${member.id}"`);
-    let value = await whmcsGet.get({}, 'GetClientsdetails', member);
     let row = await sql.get(`SELECT * FROM whmcs WHERE discordId = "${member.id}"`);
-    if (value === undefined || value.clients.client.length === 0) return Embed(msg.channel, `There was an error performing this command.`, 'error', 'Error');
-    let clientUser = value.clients.client.map(i => {
-        if (i['id'] === row.clientId) return i;
-        else return false;
-    }).filter(i => i !== false);
-    if (!row || clientUser.length === 0) return Embed(msg.channel, `${member} does not have a WHMCS account or did not link it.`, 'error', 'Error');
-    clientUser = clientUser[0];
+    if (!row) return Embed(msg.channel, `${member} does not have a linked WHMCS account.`, 'error', 'Error');
+    let clientUser = await whmcsGet.get({ clientid: row.clientId }, 'GetClientsDetails');
+    if (!clientUser) return Embed(msg.channel, `${member} does not have a WHMCS account.`, 'error', 'Error');
     Embed(msg.channel, `Email: ${clientUser['email']}\nFull name: ${clientUser['firstname']} ${clientUser['lastname']}`, 'main', `${member.user.username}#${member.user.discriminator} Client Info`)
 };
 
